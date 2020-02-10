20b1be74dceed15de8edd9d77ff3f70660c0da65
Eva Tourangeau <evatourangeau@Evas-MacBook-Pro.local>
2020-02-09 17:07:03 -0800

    cleaning timestamp variable for regression

---
+++ dataCleaning.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(chron)
+
+bike = read.csv("london_merged.csv")
+bike = separate(bike, timestamp, into = c("date", "time"), sep=" ")
+
+# Convert date/time variables to datetime/chron objects
+bike$date <- as.Date(bike$date)
+bike$time <- chron(times. = bike$time)
+
+# Extract year, month, day, hour from date/time variables
+# Note: the finest time resolution we have is hour. All minutes/seconds are "00". 
+# If curious, run below:
+# unique(minutes(bike$time))
+# unique(seconds(bike$time))
+bike <- bike %>% mutate(year = as.numeric(format(date, "%Y")), 
+                        month = as.numeric(format(date, "%m")),
+                        day = as.numeric(format(date, "%d")),
+                        hour = as.numeric(hours(time))) %>% select(c(-1,-2))
+
